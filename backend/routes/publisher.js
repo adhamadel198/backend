ff8876a73a2a3d10577505466e28742847648343
adham adel <adhamadel198@gmail.com>
2023-12-08 10:32:56 +0200

    updated version

--- backend/routes/publisher.js
+++
@@ -1,6 +0,0 @@
-const {Router}= require('express');
-const publishersController= require('../controllers/publisher');
-const publishersRouter = Router();
-publishersRouter.get('/',publishersController.getpublishers);
-module.exports= publishersRouter;
-publishersRouter.post('/',publishersController.postPublisher);
