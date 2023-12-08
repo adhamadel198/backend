50cb03d2c4374c688cf4a28cbaf99dfce2beaacb
adham adel <adhamadel198@gmail.com>
2023-12-09 01:00:32 +0200

    updated version

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const initDBConnection = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/test");
+    await mongoose.connect(process.env.MONGO_CONNECTION_URL);
     console.log("Connected to DB Server");
   } catch (error) {
     console.log(error);
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const dotenv = require('dotenv');
 const cors = require('cors');
 
 const initialdbconnection = require('./config/db');
@@ -8,11 +8,11 @@ const articlerouter = require('./routes/article');
 const publishersRouter = require('./routes/publisher')
 const draftsRouter = require('./routes/draft')
 
-
+dotenv.config({path: './config/.env',});
 
 const app = express();
 
-const port = process.env; 
+const port = process.env.PORT; 
 
 app.use(cors());
 
